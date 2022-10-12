78465eb538c6a964efdb3f8fda74238a6d94d75c
HocariFounder <115596001+HocariFounder@users.noreply.github.com>
2022-10-12 18:21:15 -0400

    Update send_report.py

--- send_report.py
+++ send_report.py
@@ -1,11 +1,11 @@
 import smtplib
 
-sender_email = 'contact@hocari.com'
-sender_password = 'Oddpaste90'
+sender_email = 'YOUR_EMAIL'
+sender_password = 'YOUR_EMAIL_PASSWORD'
 
 sent_from = sender_email
-to = ['briac@mail.com']
-subject = 'Test for Briac'
+to = ['RECIEVER_EMAIL']
+subject = 'TYPE YOUR OWN SUBJECT'
 # Next line doesn't accept accents!
 body = 'Voici un email envoye automatiquement (enfin quand tout sera termine) par Python, pour Safe Anywhere.'
 
@@ -18,7 +18,7 @@ Subject: %s
 """ % (sent_from, ", ".join(to), subject, body)
 
 try:
-    smtp_server = smtplib.SMTP_SSL('mail.privateemail.com', 465)
+    smtp_server = smtplib.SMTP_SSL('YOUR_IMAP', 465)
     smtp_server.ehlo()
     smtp_server.login(sender_email, sender_password)
     smtp_server.sendmail(sent_from, to, email_text)
